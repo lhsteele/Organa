f32309e4e86e53039c91bf6046200c7dafd69b75
Lisa Steele <lisahsteele@gmail.com>
2019-05-30 20:34:17 -0700

    Had ended up with an index on first name in the Users table (as a result of renaming it from username to first_name when I changed my auth pattern). Had to remove this index as it should be possible to have more than 1 user with the same first name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_160029) do
+ActiveRecord::Schema.define(version: 2019_05_31_033154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 2019_05_28_160029) do
     t.datetime "updated_at", null: false
     t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["first_name"], name: "index_users_on_first_name", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
